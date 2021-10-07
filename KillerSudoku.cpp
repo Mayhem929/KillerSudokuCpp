3dfc364708ab6c3101a7949739c8ced2b7687dea
Adrián Jaén Fuentes <76180710+Mayhem929@users.noreply.github.com>
2021-10-07 19:36:17 +0200

    changed getSum function algorithm
    Used recursive way of searching close cells contained in the same area. Also changed board type to int.

--- KillerSudoku.cpp
+++ KillerSudoku.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <utility>
 #include <vector>
+#include <chrono>
 
+using namespace std::chrono;
 using namespace std;
-
-const int CHAR_TO_INT = '0';
 
 class KillerSudoku{
 public:
@@ -14,9 +13,9 @@ public:
      * @param sum
      */
 
-    KillerSudoku(vector<vector<int>> areas_map, vector<int> sum){
-        areas = move(areas_map);
-        areas_sum = move(sum);
+    KillerSudoku(vector<vector<int>> areas, vector<int> areas_sum){
+        this->areas = move(areas);
+        this->areas_sum = move(areas_sum);
     }
 
     void printSudoku(){
@@ -28,66 +27,64 @@ public:
     }
 
     /**
-     * @param areas
-     * @param board
+     *
      * @param i
      * @param j
-     * @return -1 if area is not complete yet, area's sum if complete
+     * @param area_id
+     * @param shown
+     * @param sum
      */
 
-    int getSum(int i, int j, int val){
+    void getSum(int i, int j, int area_id, bool shown[9][9], int &sum){
 
-        int area_id = areas[i][j];
+        for (int x = i - 1; x <= i + 1; ++x) {
+            for (int y = j - 1; y <= j + 1; ++y) {
+                if (0 <= x && x < 9 && 0 <= y && y < 9 && !shown[x][y]) {
+                    if (areas[x][y] != area_id)continue;
+                    if (board[x][y] == 0)
+                        sum += 100;
 
-        int sum = 0;
-        board[i][j]=val;
-
-        bool shown[9][9] = {false};
-
-        for(int x = 0; x < 9; ++x){
-            for (int y = 0; y < 9; ++y) {
-                if(areas[x][y] == area_id){
-                    if (board[x][y] == board[i][j] && (x != i || y != j)){
-                        board[i][j] = 0;
-                        return -1;
-                    }
-                    if(board[x][y] == 0) {
-                        return -1;
-                    }
-                    else {
-                        sum += board[x][y];
-                    }
+                    shown[x][y] = true;
+                    sum += board[x][y];
+                    getSum(x,y,area_id,shown,sum);
                 }
             }
         }
-        board[i][j] = 0;
-        return sum;
     }
 
     /**
      *
-     * @param board
-     * @param areas_map
-     * @param sum
      * @param i
      * @param j
      * @param val
-     * @return true if value on given position is correct
+     * @return
      */
 
     bool check(int i, int j, int val)
     {
         int row = i - i%3, column = j - j%3;
-        for(int x=0; x<9; x++) if(board[x][j] == val) return false;
-        for(int y=0; y<9; y++) if(board[i][y] == val) return false;
+
+        for(int x=0; x<9; x++)
+            if(board[x][j] == val)
+                return false;
+        for(int y=0; y<9; y++)
+            if(board[i][y] == val)
+                return false;
         for(int x=0; x<3; x++)
             for(int y=0; y<3; y++)
                 if(board[row+x][column+y] == val) return false;
 
         int area_id = areas[i][j];
-        int sum = getSum(i, j, val);
+        int aux = board[i][j];
+        board[i][j] = val;
+        bool shown[9][9] = {false};
+        int sum = 0;
+        getSum(i, j, area_id, shown, sum);
 
-        if(sum != -1 && sum != areas_sum[area_id]) return false;
+        if(sum < 100 && sum != areas_sum[area_id]) {
+            board[i][j] = aux;
+            return false;
+        }
 
         return true;
     }
@@ -95,11 +92,14 @@ public:
     bool solveSudoku(int i, int j)
     {
         static int count = 0;
-        if(i==9) return true;
+        if(i==9) {
+            cout << endl << "Iterations needed: " << count << endl;
+            return true;
+        }
         if(j==9) return solveSudoku(i+1, 0);
         if(board[i][j] != 0) return solveSudoku(i, j+1);
 
-        for (int val = 9; val >= 0; --val) {
+        for (int val = 9; val > 0; --val) {
             if (check(i, j, val)) {
                 board[i][j] = val;
                 if (solveSudoku(i, j + 1)) return true;
@@ -111,6 +111,7 @@ public:
     }
 
 private:
+
 
     vector<vector<int>> areas;
     vector<int> areas_sum;
@@ -125,55 +126,18 @@ private:
                                  {0,0,0,0,0,0,0,0,0}};
 };
 
-/*
-void printSudoku(vector<vector<char>> sudoku){
-    for( int i = 0; i < sudoku.size(); ++i){
-        for (int j = 0; j < sudoku[0].size(); ++j) {
-            cout << sudoku[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
 
-bool check(vector<vector<char>> &board, int i, int j, char val)
-{
-    int row = i - i%3, column = j - j%3;
-    for(int x=0; x<9; x++) if(board[x][j] == val) return false;
-    for(int y=0; y<9; y++) if(board[i][y] == val) return false;
-    for(int x=0; x<3; x++)
-        for(int y=0; y<3; y++)
-            if(board[row+x][column+y] == val) return false;
-
-    return true;
-}
-
-bool solveSudoku(vector<vector<char>> &board, int i, int j)
-{
-    if(i==9) return true;
-    if(j==9) return solveSudoku(board, i+1, 0);
-    if(board[i][j] != '.') return solveSudoku(board, i, j+1);
-
-    for (char c = '1'; c <= '9'; c++) {
-        if (check(board, i, j, c)) {
-            board[i][j] = c;
-            printSudoku(board);
-            if (solveSudoku(board, i, j + 1)) return true;
-            board[i][j] = '.';
-        };
-    };
-
-    return false;
-}
-*/
 int main() {
 
-    vector<vector<int>> areas   = {{0, 0, 1, 2, 3, 3, 4, 5, 5},
-                                   {1, 1, 1, 2, 2, 6, 4, 5, 5},
-                                   {7, 8, 8, 8, 2, 6, 4, 9, 5},
-                                   {7, 10, 8, 8, 11, 12, 12, 9, 5},
-                                   {7, 10, 13, 13, 11, 12, 12, 14, 14},
-                                   {7, 10, 15, 15, 11, 12, 12, 16, 17},
-                                   {7, 15, 15, 15, 18, 19, 20, 16, 17},
+    auto start = high_resolution_clock::now();
+
+    vector<vector<int>> areas   = {{0 , 0 , 1 , 2 , 3 , 3 , 4 , 5 , 5 },
+                                   {1 , 1 , 1 , 2 , 2 , 6 , 4 , 5 , 5 },
+                                   {7 , 8 , 8 , 8 , 2 , 6 , 4 , 9 , 5 },
+                                   {7 , 10, 8 , 8 , 11, 12, 12, 9 , 5 },
+                                   {7 , 10, 13, 13, 11, 12, 12, 14, 14},
+                                   {7 , 10, 15, 15, 11, 12, 12, 16, 17},
+                                   {7 , 15, 15, 15, 18, 19, 20, 16, 17},
                                    {21, 21, 21, 18, 18, 19, 20, 17, 17},
                                    {22, 22, 21, 18, 23, 23, 20, 17, 17}};
 
@@ -181,7 +145,7 @@ int main() {
 
     KillerSudoku sudoku(areas, areas_sum);
 
-
+    cout << "Int implementation: " << endl << endl;
     cout << "Unsolved sudoku:" << endl;
 
     sudoku.printSudoku();
@@ -192,6 +156,12 @@ int main() {
     cout << "Solved Sudoku:" << endl;
 
     sudoku.printSudoku();
+
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << endl << endl << "Run Duration: " << duration.count() << " microsecods.";
 
     return 0;
 }
